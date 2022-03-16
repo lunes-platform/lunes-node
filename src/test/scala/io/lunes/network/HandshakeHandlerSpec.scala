ffda68f628fbab9c2c832129cdb086336783e901
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-03-16 00:49:35 -0300

    added: tests for compatible handshake of nodes

---
+++ src/test/scala/io/lunes/network/HandshakeHandlerSpec.scala
@@ -0,0 +1,49 @@
+package io.lunes.network
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class HandshakeHandlerSpec extends AnyFunSuite {
+
+  test("Version (2, 0, 0) should be compatible") {
+    assert(
+      true == HandshakeHandler.versionIsSupported((2, 0, 0))
+    )
+  }
+  test("Version (1, 21, 1) should be compatible") {
+    assert(
+      true == HandshakeHandler.versionIsSupported((1, 21, 1))
+    )
+  }
+  test("Version (0, 1, 2) should be compatible") {
+    assert(
+      true == HandshakeHandler.versionIsSupported((0, 1, 2))
+    )
+  }
+  test("Version (0, 1, 0) should be compatible") {
+    assert(
+      true == HandshakeHandler.versionIsSupported((0, 1, 0))
+    )
+  }
+
+  test("Version between (0, 0, 13) should be NOT compatible") {
+    assert(
+      false == HandshakeHandler.versionIsSupported((0, 0, 13))
+    )
+  }
+  test("Version between (0, 0, 9) should be NOT compatible") {
+    assert(
+      false == HandshakeHandler.versionIsSupported((0, 0, 9))
+    )
+  }
+  test("Version between (0, 0, 1) should be NOT compatible") {
+    assert(
+      false == HandshakeHandler.versionIsSupported((0, 0, 1))
+    )
+  }
+
+//  test("Invoking head on an empty Set should produce NoSuchElementException") {
+//    assertThrows[NoSuchElementException] {
+//      Set.empty.head
+//    }
+//  }
+}
