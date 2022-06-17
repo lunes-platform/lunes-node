265bb9cd3c63405aede1f18eb9c6cdc55ee5d799
Lucas Oliveira <olivmath@protonmail.com>
2022-06-17 13:10:20 -0300

    fix: api error, frozen to banned

--- src/main/scala/scorex/api/http/ApiError.scala
+++ src/main/scala/scorex/api/http/ApiError.scala
@@ -49,15 +49,14 @@ object ApiError {
         case ValidationError.Mistiming(errorMessage) => Mistiming(errorMessage)
         case _                                       => StateCheckFailed(tx, fromValidationError(error).message)
       }
-    case ValidationError.FrozenAssetTransaction(x) =>
-      FrozenAssetTransaction(x)
-    case error => CustomValidationError(error.toString)
+    case ValidationError.BannedAddress(x) => BannedAddress(x)
+    case error                            => CustomValidationError(error.toString)
   }
 }
 
 case object Unknown extends ApiError {
-  override val id = 0
-  override val code = StatusCodes.InternalServerError
+  override val id      = 0
+  override val code    = StatusCodes.InternalServerError
   override val message = "Error is unknown"
 }
 
@@ -65,144 +64,144 @@ case class WrongJson(
   cause: Option[Throwable] = None,
   errors: Seq[(JsPath, Seq[JsonValidationError])] = Seq.empty
 ) extends ApiError {
-  override val id = 1
-  override val code = StatusCodes.BadRequest
+  override val id           = 1
+  override val code         = StatusCodes.BadRequest
   override lazy val message = "failed to parse json message"
   override lazy val json: JsObject = Json.obj(
-    "error" -> id,
-    "message" -> message,
-    "cause" -> cause.map(_.toString),
+    "error"            -> id,
+    "message"          -> message,
+    "cause"            -> cause.map(_.toString),
     "validationErrors" -> JsError.toJson(errors)
   )
 }
 
 //API Auth
 case object ApiKeyNotValid extends ApiError {
-  override val id = 2
-  override val code = StatusCodes.Forbidden
+  override val id              = 2
+  override val code            = StatusCodes.Forbidden
   override val message: String = "Provided API key is not correct"
 }
 
 case object DiscontinuedApi extends ApiError {
-  override val id = 3
-  override val code = StatusCodes.BadRequest
+  override val id      = 3
+  override val code    = StatusCodes.BadRequest
   override val message = "This API is no longer supported"
 }
 
 case object TooBigArrayAllocation extends ApiError {
-  override val id: Int = 10
-  override val message: String = "Too big sequences requested"
+  override val id: Int          = 10
+  override val message: String  = "Too big sequences requested"
   override val code: StatusCode = StatusCodes.BadRequest
 }
 
 //VALIDATION
 case object InvalidSignature extends ApiError {
-  override val id = 101
-  override val code = StatusCodes.BadRequest
+  override val id      = 101
+  override val code    = StatusCodes.BadRequest
   override val message = "invalid signature"
 }
 
 case object InvalidAddress extends ApiError {
-  override val id = 102
-  override val code = StatusCodes.BadRequest
+  override val id      = 102
+  override val code    = StatusCodes.BadRequest
   override val message = "invalid address"
 }
 
 case object InvalidSeed extends ApiError {
-  override val id = 103
-  override val code = StatusCodes.BadRequest
+  override val id      = 103
+  override val code    = StatusCodes.BadRequest
   override val message = "invalid seed"
 }
 
 case object InvalidAmount extends ApiError {
-  override val id = 104
-  override val code = StatusCodes.BadRequest
+  override val id      = 104
+  override val code    = StatusCodes.BadRequest
   override val message = "invalid amount"
 }
 
 case object InvalidFee extends ApiError {
-  override val id = 105
-  override val code = StatusCodes.BadRequest
+  override val id      = 105
+  override val code    = StatusCodes.BadRequest
   override val message = "invalid fee"
 }
 
 case object InvalidSender extends ApiError {
-  override val id = 106
-  override val code = StatusCodes.BadRequest
+  override val id      = 106
+  override val code    = StatusCodes.BadRequest
   override val message = "invalid sender"
 }
 
 case object InvalidRecipient extends ApiError {
-  override val id = 107
-  override val code = StatusCodes.BadRequest
+  override val id      = 107
+  override val code    = StatusCodes.BadRequest
   override val message = "invalid recipient"
 }
 
 case object InvalidPublicKey extends ApiError {
-  override val id = 108
-  override val code = StatusCodes.BadRequest
+  override val id      = 108
+  override val code    = StatusCodes.BadRequest
   override val message = "invalid public key"
 }
 
 case object InvalidNotNumber extends ApiError {
-  override val id = 109
-  override val code = StatusCodes.BadRequest
+  override val id      = 109
+  override val code    = StatusCodes.BadRequest
   override val message = "argument is not a number"
 }
 
 case object InvalidMessage extends ApiError {
-  override val id = 110
-  override val code = StatusCodes.BadRequest
+  override val id      = 110
+  override val code    = StatusCodes.BadRequest
   override val message = "invalid message"
 }
 
 case object InvalidName extends ApiError {
-  override val id: Int = 111
-  override val message: String = "invalid name"
+  override val id: Int          = 111
+  override val message: String  = "invalid name"
   override val code: StatusCode = StatusCodes.BadRequest
 }
 
 case class StateCheckFailed(tx: Transaction, err: String) extends ApiError {
-  override val id: Int = 112
-  override val message: String = s"State check failed. Reason: $err"
+  override val id: Int          = 112
+  override val message: String  = s"State check failed. Reason: $err"
   override val code: StatusCode = StatusCodes.BadRequest
   override lazy val json =
     Json.obj("error" -> id, "message" -> message, "tx" -> tx.json())
 }
 
 case object OverflowError extends ApiError {
-  override val id: Int = 113
-  override val message: String = "overflow error"
+  override val id: Int          = 113
+  override val message: String  = "overflow error"
   override val code: StatusCode = StatusCodes.BadRequest
 }
 
 case object ToSelfError extends ApiError {
-  override val id: Int = 114
-  override val message: String = "Transaction to yourself"
+  override val id: Int          = 114
+  override val message: String  = "Transaction to yourself"
   override val code: StatusCode = StatusCodes.BadRequest
 }
 
 case object MissingSenderPrivateKey extends ApiError {
-  override val id: Int = 115
-  override val message: String = "no private key for sender address in wallet"
+  override val id: Int          = 115
+  override val message: String  = "no private key for sender address in wallet"
   override val code: StatusCode = StatusCodes.BadRequest
 }
 
 case class CustomValidationError(errorMessage: String) extends ApiError {
-  override val id: Int = 199
-  override val message: String = errorMessage
+  override val id: Int          = 199
+  override val message: String  = errorMessage
   override val code: StatusCode = StatusCodes.BadRequest
 }
 
 case object BlockNotExists extends ApiError {
-  override val id: Int = 301
-  override val code = StatusCodes.NotFound
+  override val id: Int         = 301
+  override val code            = StatusCodes.NotFound
   override val message: String = "block does not exist"
 }
 
 case class AliasNotExists(aoa: AddressOrAlias) extends ApiError {
   override val id: Int = 302
-  override val code = StatusCodes.NotFound
+  override val code    = StatusCodes.NotFound
   private lazy val msgReason = aoa match {
     case a: Address => s"for address '${a.stringRepr}'"
     case a: Alias   => s"'${a.stringRepr}'"
@@ -211,8 +210,8 @@ case class AliasNotExists(aoa: AddressOrAlias) extends ApiError {
 }
 
 case class Mistiming(errorMessage: String) extends ApiError {
-  override val id: Int = Mistiming.Id
-  override val message: String = errorMessage
+  override val id: Int          = Mistiming.Id
+  override val message: String  = errorMessage
   override val code: StatusCode = StatusCodes.BadRequest
 }
 
@@ -220,9 +219,8 @@ object Mistiming {
   val Id = 303
 }
 
-case class FrozenAssetTransaction(msg: String) extends ApiError {
-  override val id: Int = 116
-  override val message: String =
-    s"The Request Transaction Asset has been issued with a Freeze Order: $msg"
+case class BannedAddress(msg: List[String]) extends ApiError {
+  override val id: Int          = 666
+  override val message: String  = s"🚨 Address ${msg} are banned."
   override val code: StatusCode = StatusCodes.BadRequest
 }
